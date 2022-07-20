af52597fbbf30b3125432bf288fb88d4fa89642e
RRDaniel <54445510+RRDaniel@users.noreply.github.com>
2022-07-20 03:40:08 -0300

    Create Ex 003.py

---
+++ Ex 003.py
@@ -0,0 +1,5 @@
+# Ex 003: Crie um programa que leia dois números e mostre a soma entre eles.
+
+n1 = int(input('Informe o 1° numero : '))
+n2 = int(input('Informe o 2° numero : '))
+print(f'{n1} + {n2} = {n1 + n2}')
