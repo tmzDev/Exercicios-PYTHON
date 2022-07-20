ca63d891727ca0e98e30104f23406a591ab69d7a
Daniel Rios <54445510+RRDaniel@users.noreply.github.com>
2022-07-20 17:27:39 -0300

    Create Ex 009.py

---
+++ Ex 009.py
@@ -0,0 +1,8 @@
+# Ex 009: Faça um programa que leia um número Inteiro qualquer e mostre na tela a sua tabuada
+
+t = int(input('Tabuada : '))
+for i in range(1,11):
+    print(f'{i} x {t} = {i * t}')
+
+# Acompanhando pelo curso, laços ainda não foram mostrados, porém estou com
+# preguiça de fazer print do 1 ao 10 :p
