a75a73b6800d07489d88381099376f489286d5f9
RRDaniel <54445510+RRDaniel@users.noreply.github.com>
2022-07-20 04:56:14 -0300

    Create Ex 005.py

---
+++ Ex 005.py
@@ -0,0 +1,4 @@
+# Ex 005: Faça um programa que leia um número Inteiro e mostre na tela o seu sucessor e seu antecessor
+
+n = int(input('Informe um numero : '))
+print(f'Antecessor : {n - 1} \nSucessor : {n + 1}') 
