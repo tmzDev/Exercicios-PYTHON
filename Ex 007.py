8860cadd5d708fa9c45f1134ee25b11d82816fae
RRDaniel <54445510+RRDaniel@users.noreply.github.com>
2022-07-20 05:12:25 -0300

    Create Ex 007.py

---
+++ Ex 007.py
@@ -0,0 +1,5 @@
+# Ex 007: Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média.
+
+n1 = float(input('Primeira nota : ' ))
+n2 = float(input('Segunda nota : '))
+print(f'A media entre {n1} e {n2} é {(n1 + n2) / 2:.1f}')
