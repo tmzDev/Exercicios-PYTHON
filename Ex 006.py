f94bf67d1790368a4cf2adf4e6d92f3d079f4c38
RRDaniel <54445510+RRDaniel@users.noreply.github.com>
2022-07-20 04:56:54 -0300

    Create Ex 006.py

---
+++ Ex 006.py
@@ -0,0 +1,4 @@
+# Ex 006: Crie um algoritmo que leia um número e mostre o seu dobro, triplo e raiz quadrada.
+
+n = int(input('Digite um numero : '))
+print(f'Dobro {n * 2} \nTriplo {n * 3} \nRaiz {n ** 0.5:.0f}')
