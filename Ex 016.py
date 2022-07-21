8ce1b715e256c9cc47336be6f8b700130cf28309
Daniel Rios <54445510+RRDaniel@users.noreply.github.com>
2022-07-21 05:38:28 -0300

    Create Ex 016.py

---
+++ Ex 016.py
@@ -0,0 +1,4 @@
+# Ex 016: Crie um programa que leia um número Real qualquer pelo teclado e mostre na tela a sua porção Inteira.
+
+n = float(input('Digite um numero : '))
+print(f'{n} a parte inteira é {round(n)}')
