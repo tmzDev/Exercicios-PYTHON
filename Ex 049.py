4c57da145ca92bd50bce62dca4185f2e75618ca6
Daniel Rios <54445510+RRDaniel@users.noreply.github.com>
2022-07-28 19:49:11 -0300

    Create Ex 049.py

---
+++ Ex 049.py
@@ -0,0 +1,5 @@
+# Exercício Python 049: Refaça o DESAFIO 009, mostrando a tabuada de um número que o usuário escolher, só que agora utilizando um laço for.
+
+n = int(input('Informe um numero : '))
+for i in range(1, 11):
+    print(f'{i} x {n} = {i * n}')
